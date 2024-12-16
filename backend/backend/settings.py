c9c38458c0ba987a65f88ab20652c8ae55e367bd
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2024-12-16 18:09:52 +0100

    feat(DB): add of local database OK

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +124,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+env = environ.Env()
+environ.Env.read_env()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('RRPM_DB_NAME'),
+        'USER': env('RRPM_DB_USER'),
+        'PASSWORD': env('RRPM_DB_PASSWORD'),
+    }
+}
