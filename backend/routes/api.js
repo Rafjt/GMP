02e07b301cf68f04c190218124e02db2916eede0
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2025-03-06 09:10:16 +0100

    route modif master_pwd OK

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -9,7 +9,7 @@ router.get('/about', (req, res) => {
     res.send('About this app');
 });
 
-router.get('/all_users', async (req, res) => {
+router.get('/all_user', async (req, res) => {
     try {
         const response = await sequelize.query(
           `SELECT * FROM rrpm_user`
@@ -18,6 +18,24 @@ router.get('/all_users', async (req, res) => {
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({error: 'Internal server error'});
+    }
+});
+
+router.put('/master/password/:id', async (req, res) => {
+    const { id } = req.params;
+    const { password } = req.body;
+    try {
+        const response = await sequelize.query(
+            'UPDATE rrpm_user SET hashed_master_password = :password WHERE id = :id',
+            {
+                replacements: { password, id },
+                type: sequelize.QueryTypes.UPDATE
+            }
+        );
+        res.json({ message: 'Password updated successfully' });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 });
 
