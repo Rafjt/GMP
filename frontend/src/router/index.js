8750341ee11c7a8fbbf6119cfad4bcb1c1e28129
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2025-03-15 14:13:43 +0100

    UI improve

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginView from '@/views/LoginView.vue';
 import WelcomeView from '@/views/WelcomeView.vue';
+import CreateAccountView from '@/views/CreateAccountView.vue';
 
 const routes = [
   { path: '/', component: LoginView },
-  { path: '/welcome', component: WelcomeView }
+  { path: '/welcome', component: WelcomeView },
+  { path: '/create', component: CreateAccountView }
 ];
 
 const router = createRouter({
