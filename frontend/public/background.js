c2f9f50d6e73c483c6abb6b7f512b991e8be2e76
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2025-04-01 19:40:57 +0200

    add of loggout route

--- frontend/public/background.js
+++ frontend/public/background.js
@@ -1,7 +1,7 @@
-chrome.cookies.get({ url: "http://localhost", name: "token" }, function (cookie) {
-    if (cookie) {
-        console.log("Token:", cookie.value);
-    } else {
-        console.log("CSRF Token non trouvé !");
-    }
-});
+// chrome.cookies.get({ url: "http://localhost", name: "token" }, function (cookie) {
+//     if (cookie) {
+//         console.log("Token:", cookie.value);
+//     } else {
+//         console.log("CSRF Token non trouvé !");
+//     }
+// });
