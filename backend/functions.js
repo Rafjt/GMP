5ae91d73b27d90f29d55b2f57c9e7acd7e5e6c68
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2025-06-19 10:45:18 +0200

    routes hardening

--- backend/functions.js
+++ backend/functions.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const Limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 5, // Limit each IP to 5 requests per windowMs
+  max: 20, // Limit each IP to 5 requests per windowMs
   message: { message: "Too many login attempts. Please try again later." },
   standardHeaders: true, // Return rate limit info in headers
   legacyHeaders: false,
