26a0b0480eb25db5aa7dcc274f2e87ea5b4976b0
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2025-07-01 17:09:28 +0200

    visual addition to setting view

--- frontend/src/views/SettingView.vue
+++ frontend/src/views/SettingView.vue
@@ -1,3 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+
+const showPasswordFields = ref(false)
+const oldPassword = ref('')
+const newPassword = ref('')
+
+const togglePasswordFields = () => {
+  showPasswordFields.value = !showPasswordFields.value
+}
+</script>
+
 <template>
-    <p>hello world</p>
+  <div class="setting-container">
+    <div class="password-change">
+      <button class="button-event" @click="togglePasswordFields">
+        {{ showPasswordFields ? 'Cancel' : 'Change master password' }}
+      </button>
+
+      <div v-if="showPasswordFields" class="mt-4 space-y-2">
+        <label for="old-password" class="old-new-password-label">Old password</label>
+        <input
+          v-model="oldPassword"
+          type="password"
+          class="old-new-password"
+        />
+        <label for="new-password" class="old-new-password-label">New password</label>
+        <input
+          v-model="newPassword"
+          type="password"
+          class="old-new-password"
+        />
+      </div>
+    </div>
+
+    <div class="account-deletion mt-6">
+      <button class="button-event-critical">DELETE YOUR ACCOUNT</button>
+      <p class="deletion-warning">
+        ⚠️ Deleting your account is a definitive action, but you will instantly be able to re-create another account with the same E-mail
+      </p>
+    </div>
+  </div>
 </template>
+
+<style scoped src="@/assets/main.css"></style>
